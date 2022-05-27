4b45a034268962f4b213d6d65fcbc81bfe6ac048
DevLop-EC <christlopbsc12@gmail.com>
2022-05-27 18:52:46 -0500

    Modle for post publications

---
+++ server/models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = Schema({
+  idUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+
+  file: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  typeFile: {
+    type: String,
+    trim: true,
+  },
+
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
